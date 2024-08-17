7b43543678fdad5c797e280154b4aefbf71bd554
Sfer7 <ffdfig@gmail.com>
2024-08-17 22:41:45 +0900

    Remove: draft.js 제거

--- src/app/_components/_templates/_textEditor/RecordTextEditor.tsx
+++ src/app/_components/_templates/_textEditor/RecordTextEditor.tsx
@@ -1,46 +1,5 @@
+"use client"
+
 import styles from "./RecordTextEditor.module.css"
-import React, {useEffect, useState} from 'react'
-import {Editor, EditorState, RichUtils, DraftEditorCommand, DraftHandleValue} from 'draft-js'
-import 'draft-js/dist/Draft.css'
 
-const RecordTextEditor = () => {
 
-    useEffect(() => {
-        const initialState = EditorState.createEmpty();
-        setEditorState(initialState);
-    }, []);
-
-    const [editorState, setEditorState] = useState<EditorState | null>(null);
-
-    const hashKeyCommand = (
-        command: DraftEditorCommand,
-        editorState: EditorState
-    ): DraftHandleValue => {
-        const newState = RichUtils.handleKeyCommand(editorState, command);
-        if(newState) {
-            setEditorState(newState);
-            return 'handled';
-        }
-        return 'not-handled';
-    }
-
-    if(!editorState) {
-        return null;
-    }
-
-    return (
-        <>
-            <div className={styles.editorWrapper}>
-                <Editor
-                    editorState={editorState}
-                    onChange={setEditorState}
-                    handleKeyCommand={hashKeyCommand}
-                />
-            </div>
-
-        </>
-    )
-
-}
-
-export default RecordTextEditor;
